4a47386f779ff2079a03d8d8c85acd9fc859a9a0
Lumír Lichý <lumlich@gmail.com>
2025-10-03 10:33:33 +0200

    chore(ingest): export generic_rss provider module

--- src/ingest/providers/mod.rs
+++ src/ingest/providers/mod.rs
@@ -2,6 +2,7 @@
 
 pub mod fed_rss;
 pub mod reuters_rss;
+pub mod generic_rss;
 
 // Re-export trait and event type so provider modules can use `super::{Provider, ProviderItem};`
 pub use crate::ingest::types::{SourceEvent, SourceProvider as Provider};
